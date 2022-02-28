7bda87cc058d0a06b574262014b43a31116313fb
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-02-27 21:37:59 -0500

    linked module & created algorithm structure

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -9,7 +10,12 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const fileName = '';
+    writeToFile(fileName, answers)
+  })
+}
 
 // Function call to initialize app
 init();
